c508dfd1e5fdf6aa35d433d616c340b19ac1c1e9
ddnosh <ddnosh@qq.com>
2020-02-22 17:08:38 +0800

    增加一些小示例

--- tinymvp-sample/src/main/java/com/androidwind/mvp/sample/autodispose/AutodisposeModel.java
+++ tinymvp-sample/src/main/java/com/androidwind/mvp/sample/autodispose/AutodisposeModel.java
@@ -2,7 +2,11 @@ package com.androidwind.mvp.sample.autodispose;
 
 import com.androidwind.mvp.sample.disposable.DisposableContract;
 
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.functions.Function;
 
 /**
  * @author ddnosh
@@ -12,15 +16,22 @@ public class AutodisposeModel implements AutodisposeContract.Model {
 
     @Override
     public Observable<Boolean> executeMSg() {
-        return Observable.create(emitter -> {
-                    try {
-                        Thread.sleep(2000); // 假设此处是耗时操作
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        emitter.onError(new RuntimeException());
+        // return Observable.create(emitter -> {
+        //             try {
+        //                 Thread.sleep(2000); // 假设此处是耗时操作
+        //             } catch (Exception e) {
+        //                 e.printStackTrace();
+        //                 emitter.onError(new RuntimeException());
+        //             }
+        //             emitter.onNext(true);
+        //         }
+        return Observable.timer(2, TimeUnit.SECONDS)
+                .flatMap(new Function<Long, ObservableSource<Boolean>>() {
+                    @Override
+                    public ObservableSource<Boolean> apply(Long aLong) throws Exception {
+                        return Observable.just(true);
                     }
-                    emitter.onNext(true);
-                }
-        );
+                });
+        // );
     }
 }
